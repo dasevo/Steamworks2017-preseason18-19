0b6bd1ddf39f716b2cddd61ed82271bf89d89b12
dasevo <43734555+dasevo@users.noreply.github.com>
2018-10-22 13:22:11 -0500

    Update Command_Base.java

--- src/org/usfirst/frc/team3630/robot/commands/Command_Base.java
+++ src/org/usfirst/frc/team3630/robot/commands/Command_Base.java
@@ -5,7 +5,7 @@ import org.usfirst.frc.team3630.robot.subsystems.MecanumDrive_Subsystem;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public abstract class Command_Base extends Command {
+public abstract class Command_Base extends Command { //I don't really know what is this for but it seems that it is supposed to be here
 	
 	public static OI oi;
 	public static MecanumDrive_Subsystem driveMecanum = new MecanumDrive_Subsystem();
